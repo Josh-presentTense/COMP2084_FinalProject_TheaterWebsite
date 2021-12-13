aff90b4050b9c5a1bf3dd8552163a53fd9492e40
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-12-13 00:19:40 -0500

    Add project files.

---
+++ TheaterWebsiteProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheaterWebsiteProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
