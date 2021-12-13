eee32f2d64dd881352580656a73cd8b5a0071dae
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-12-13 05:59:20 -0500

    Added Controllers for Movies, Tickets, Snacks, Experiences. Updated the created Views for the mentioned Models.

---
+++ TheaterWebsiteProject/Controllers/SnacksController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TheaterWebsiteProject.Models;
+
+namespace TheaterWebsiteProject.Controllers
+{
+    public class SnacksController : Controller
+    {
+        private readonly DataContext _context;
+
+        public SnacksController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Snacks
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = _context.Snacks.Include(s => s.Ticket);
+            return View(await dataContext.ToListAsync());
+        }
+
+        // GET: Snacks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var snack = await _context.Snacks
+                .Include(s => s.Ticket)
+                .FirstOrDefaultAsync(m => m.SnackID == id);
+            if (snack == null)
+            {
+                return NotFound();
+            }
+
+            return View(snack);
+        }
+
+        // GET: Snacks/Create
+        public IActionResult Create()
+        {
+            ViewData["TicketID"] = new SelectList(_context.Tickets, "TicketID", "TicketID");
+            return View();
+        }
+
+        // POST: Snacks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SnackID,Food,Drink,Candy,TicketID")] Snack snack)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(snack);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TicketID"] = new SelectList(_context.Tickets, "TicketID", "TicketID", snack.TicketID);
+            return View(snack);
+        }
+
+        // GET: Snacks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var snack = await _context.Snacks.FindAsync(id);
+            if (snack == null)
+            {
+                return NotFound();
+            }
+            ViewData["TicketID"] = new SelectList(_context.Tickets, "TicketID", "TicketID", snack.TicketID);
+            return View(snack);
+        }
+
+        // POST: Snacks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SnackID,Food,Drink,Candy,TicketID")] Snack snack)
+        {
+            if (id != snack.SnackID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(snack);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SnackExists(snack.SnackID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TicketID"] = new SelectList(_context.Tickets, "TicketID", "TicketID", snack.TicketID);
+            return View(snack);
+        }
+
+        // GET: Snacks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var snack = await _context.Snacks
+                .Include(s => s.Ticket)
+                .FirstOrDefaultAsync(m => m.SnackID == id);
+            if (snack == null)
+            {
+                return NotFound();
+            }
+
+            return View(snack);
+        }
+
+        // POST: Snacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var snack = await _context.Snacks.FindAsync(id);
+            _context.Snacks.Remove(snack);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SnackExists(int id)
+        {
+            return _context.Snacks.Any(e => e.SnackID == id);
+        }
+    }
+}
---
+++ TheaterWebsiteProject/Controllers/TicketsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TheaterWebsiteProject.Models;
+
+namespace TheaterWebsiteProject.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TicketsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tickets
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = _context.Tickets.Include(t => t.Movie).Include(t => t.Theater);
+            return View(await dataContext.ToListAsync());
+        }
+
+        // GET: Tickets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.Tickets
+                .Include(t => t.Movie)
+                .Include(t => t.Theater)
+                .FirstOrDefaultAsync(m => m.TicketID == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // GET: Tickets/Create
+        public IActionResult Create()
+        {
+            ViewData["MovieID"] = new SelectList(_context.Movies, "MovieID", "MovieID");
+            ViewData["TheaterID"] = new SelectList(_context.Theaters, "TheaterID", "TheaterID");
+            return View();
+        }
+
+        // POST: Tickets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TicketID,TicketDate,Category,MovieID,TheaterID")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ticket);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MovieID"] = new SelectList(_context.Movies, "MovieID", "MovieID", ticket.MovieID);
+            ViewData["TheaterID"] = new SelectList(_context.Theaters, "TheaterID", "TheaterID", ticket.TheaterID);
+            return View(ticket);
+        }
+
+        // GET: Tickets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.Tickets.FindAsync(id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+            ViewData["MovieID"] = new SelectList(_context.Movies, "MovieID", "MovieID", ticket.MovieID);
+            ViewData["TheaterID"] = new SelectList(_context.Theaters, "TheaterID", "TheaterID", ticket.TheaterID);
+            return View(ticket);
+        }
+
+        // POST: Tickets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TicketID,TicketDate,Category,MovieID,TheaterID")] Ticket ticket)
+        {
+            if (id != ticket.TicketID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ticket);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TicketExists(ticket.TicketID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MovieID"] = new SelectList(_context.Movies, "MovieID", "MovieID", ticket.MovieID);
+            ViewData["TheaterID"] = new SelectList(_context.Theaters, "TheaterID", "TheaterID", ticket.TheaterID);
+            return View(ticket);
+        }
+
+        // GET: Tickets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.Tickets
+                .Include(t => t.Movie)
+                .Include(t => t.Theater)
+                .FirstOrDefaultAsync(m => m.TicketID == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ticket = await _context.Tickets.FindAsync(id);
+            _context.Tickets.Remove(ticket);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TicketExists(int id)
+        {
+            return _context.Tickets.Any(e => e.TicketID == id);
+        }
+    }
+}
