fa18f5815da8c4e4a417cdac92742fee6f1062c4
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-12-13 21:41:26 -0500

    Set up user Register and Login

--- TheaterWebsiteProject/Data/ApplicationDbContext.cs
+++ TheaterWebsiteProject/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace TheaterWebsiteProject.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
